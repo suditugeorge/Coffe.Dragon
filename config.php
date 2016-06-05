c6d1b6558507f41c65062c7ce25b4664aeb5b9b0
Suditu George <suditugeorge94@gmail.com>
2016-06-05 12:21:37 +0300

    added hello world test to project to see if the framework works good

---
+++ config.php
@@ -0,0 +1,48 @@
+<?php
+
+define('ROOT_DIR', __DIR__);
+
+// Kickstart the framework
+/*
+ * @var Base
+ */
+
+$f3 = \Base::instance();
+
+$f3->set('UI', ROOT_DIR . '/ui/html/');
+
+$f3->set('DEBUG', false);
+
+$f3->set('AUTOLOAD', ROOT_DIR . '/classes/');
+
+define('IMAGES_DIR', '/images/');
+
+define('DATA_DIR', '/export/www/be_new/data/');
+
+define('JACKET_LARGE_WIDTH', 300);
+define('JACKET_LARGE_HEIGHT', 300);
+define('JACKET_THUMB_WIDTH', 225);
+define('JACKET_THUMB_HEIGHT', 225);
+define('JACKET_SMALL_WIDTH', 120);
+define('JACKET_SMALL_HEIGHT', 120);
+define('JACKET_XSMALL_WIDTH', 55);
+define('JACKET_XSMALL_HEIGHT', 55);
+
+define('JACKET_BANNER_WIDTH', 86);
+define('JACKET_BANNER_HEIGHT', 86);
+
+define('JACKET_EXTRA_WIDTH', 300);
+define('JACKET_EXTRA_HEIGHT', 500);
+
+//$mongo = new MongoClient();
+
+//$f3->set('MONGO', $mongo);
+$f3->set('LANGUAGE', 'ro-RO');
+
+
+$logger = new \Log('debug.log');
+function debug($message)
+{
+    global $logger;
+    $logger->write($message);
+}
